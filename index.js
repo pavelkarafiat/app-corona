0e8347761831f4158dede2e86a2a789316334e6e
pavelkarafiat <kasarar@seznam.cz>
2020-04-01 17:28:01 +0200

    custom country selection

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ app.listen(port, () => {console.log(`Starting server at ${port}`);});
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-app.get('/api', async (req, res) => {
-  const source = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
-  const urlTimeSerie = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
-  const response = await fetch(urlTimeSerie);
+app.get('/confirmed', async (req, res) => {
+  const confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+  const response = await fetch(confirmed);
   const data = await response.text();
   res.send(data);
 });
--- public/corona.js
+++ public/corona.js
@@ -1,108 +1,125 @@
-
-async function getData(){
-    try {
-        const response = await fetch('/api');
-        const text = await response.text();
-        return Promise.resolve(text);
-      } catch (error) {
-        console.error(error);
-    }
+async function getData() {
+    const response = await fetch('/confirmed');
+    const text = await response.text();
+    return text;
 }
 
-async function getCsv() {
-    const data = await getData();
-    const table = data.split('\n');
-    const dates = table[0].split(',').slice(40);
-    const infected = table[92].split(',').slice(40);
-    return {dates, infected};
+const rawdata = getData();
+
+async function getCountries() {
+    const data = await rawdata;
+    const rows = data.split('\n');
+    const countries = [];
+    rows.forEach(el => {
+        const col = el.split(',');
+        const country = col[1];
+        countries.push(country);
+    })
+    countries.shift();
+    return countries;
 }
 
-async function getChanges(){
-    const data = await getCsv();
-    const abs = data.infected;
-    let rel = [];
-    let perc = [];
+async function getCountryIndex(country) {
+    const countries = await getCountries();
+    const countryIndex = countries.findIndex(el => el == country) + 1;
+    return countryIndex;
+}
+
+async function getDetails(country) {
+    const data = await rawdata;
+    const rows = data.split('\n');
+    const countryIndex = await getCountryIndex(country);
+
+    const dates = rows[0].split(',').slice(40);
+    const confirmed = rows[countryIndex].split(',').slice(40);
+    return {dates,confirmed};
+}
+
+async function getChanges(country) {
+    const data = await getDetails(country);
+    const abs = data.confirmed;
+    let relative = [];
+    let percentual = [];
     for (let i = 0; i < abs.length; i++) {
-        rel[0]=0;
-        perc[0]=0;
-        rel[i] = abs[i]-abs[i-1];
-        perc[i]= rel[i]/abs[i]*100;
+        relative[0] = 0;
+        percentual[0] = 0;
+        relative[i] = abs[i] - abs[i - 1];
+        percentual[i] = relative[i] / abs[i] * 100;
     }
-    return {rel,perc};
+    return {relative,percentual};
+}
+
+
+function createOptions(labels, datasetlabel, data) {
+    return graphOptions = {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: datasetlabel,
+                data: data,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+    };
+}
+
+async function drawChart(country) {
+    const data = await getDetails(country);
+    const datachanges = await getChanges(country);
+    const ctx1 = document.getElementById('chart1').getContext('2d');
+    const ctx2 = document.getElementById('chart2').getContext('2d');
+    const ctx3 = document.getElementById('chart3').getContext('2d');
+    const graphoptions1 = createOptions(data.dates,'total confirmed cases',data.confirmed);
+    const graphoptions2 = createOptions(data.dates,'new cases',datachanges.relative);
+    const graphoptions3 = createOptions(data.dates,'percentual changes',datachanges.percentual);
+
+    if (window.bar1 != undefined) {window.bar1.destroy();}
+    window.bar1 = new Chart(ctx1, graphoptions1);
+    if (window.bar2 != undefined) {window.bar2.destroy();}
+    window.bar2 = new Chart(ctx2, graphoptions2);
+    if (window.bar3 != undefined) {window.bar3.destroy();}
+    window.bar3 = new Chart(ctx3, graphoptions3);
+}
+
+
+
+const defaultCountry = 'Czechia';
+let country = defaultCountry;
+const countryHeader = document.getElementById('country');
+countryHeader.textContent = ` ${country}`;
+
+drawChart(country);
+
+//add selecting of countries form
+getCountries().then(countries => {
+    const countryOption = document.getElementById('countrySelect');
+    countries.forEach(country => {
+        const entry = document.createElement('option');
+        entry.innerHTML = country;
+        entry.value = country;
+        countryOption.append(entry);
+    });
+    countryOption.addEventListener('change', (event) => {
+        country = event.target.value;
+        countryHeader.textContent = ` ${country}`;
+        drawChart(country);
+    });
+});
+
+
+
+/*
+ function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
     }
-
-
-async function chart1() {
-
-    const data = await getCsv();
-    const ctx = document.getElementById('chart1').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: data.dates,
-            datasets: [{
-                label: 'total confirmed cases in cz',
-                data: data.infected,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1,
-                fill: false
-            }]
-        },
-    });
+    return indexes;
 }
-
-async function chart2() {
-    
-    const data = await getCsv();
-    const datach = await getChanges();
-    const ctx = document.getElementById('chart2').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: data.dates,
-            datasets: [{
-                label: 'new cases in cz',
-                data: datach.rel,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1,
-                fill: false
-            }]
-        },
-    });
-}
-
-async function chart3() {
-    
-    const data = await getCsv();
-    const datach = await getChanges();
-    const ctx = document.getElementById('chart3').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: data.dates,
-            datasets: [{
-                label: 'percentual changes in cz',
-                data: datach.perc,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1,
-                fill: false
-            }]
-        },
-    });
-}
+*/
 
 
-const msg = async function() {
-    const msg = await relativeChanges();
-    console.log('Message:', msg);
-}
-
-
-chart1();
-chart2();
-chart3();
-
- 
